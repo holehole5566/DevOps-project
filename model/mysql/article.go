fdb7cd61754b483747c6e89af95a2acd44965127
holehole5566 <jd965303@gmail.com>
2021-12-22 22:19:34 +0800

    are you ok

--- model/mysql/article.go
+++ model/mysql/article.go
@@ -2,9 +2,6 @@ package mysql
 
 import (
 	"database/sql"
-	"errors"
-	"time"
-
 	"github.com/holehole5566/goproject/model"
 	"github.com/holehole5566/goproject/repo"
 )
@@ -19,63 +16,52 @@ func NewArticleRepository(db *sql.DB) repo.ArticleRepo {
 
 func (m *mysqlArticleRepository) Get(id int) (*model.Article, error) {
 
-	scanID := 0
+	scanContent := ""
 	scanTitle := ""
-	if err := m.db.QueryRow("SELECT id, title FROM goblog WHERE id = ? AND deleted_at IS NULL", id).Scan(&scanID, &scanTitle); err != nil {
-		return nil, err
-	} else if scanID != id {
-		return nil, errors.New("scan id and param id are not matched")
-	}
 
-	rows, err := m.db.Query(`SELECT collects.title, collects.id FROM collects 
-								INNER JOIN tour_collects ON collects.id = tour_collects.collect_id 
-								AND tour_collects.tour_id = ?`, id)
+	rows, err := m.db.Query(`SELECT title,content from goblog where id = ?`, id)
 	if err != nil {
 		return nil, err
 	}
 	defer rows.Close()
 
-	var tour model.Article
-	tour.ID = id
-	tour.Title = scanTitle
+	var article model.Article
+	article.Content = scanContent
+	article.Title = scanTitle
 	for rows.Next() {
-		title, id := "", 0
-		rows.Scan(&title, &id)
+		title, content := "", ""
+		rows.Scan(&title, &content)
 	}
-	return &tour, nil
-}
-
-func (m *mysqlArticleRepository) GetTotal() (int, error) {
-	count := 0
-	err := m.db.QueryRow("SELECT COUNT(*) FROM goblog WHERE deleted_at IS NULL").Scan(&count)
-	return count, err
+	return &article, nil
 }
 
 func (m *mysqlArticleRepository) Gets() (goblog []*model.Article, err error) {
-	rows, err := m.db.Query(`SELECT goblog.id, goblog.title FROM goblog WHERE deleted_at IS NULL`)
+	rows, err := m.db.Query(`SELECT goblog.id, goblog.title,goblog.content FROM goblog`)
 	if err != nil {
 		return nil, err
 	}
 	defer rows.Close()
 	for rows.Next() {
+		var content string
 		var tid int
 		var title string
-		if err := rows.Scan(&tid, &title); err != nil {
+		if err := rows.Scan(&tid, &title,&content); err != nil {
 			return nil, err
 		}
-		goblog = append(goblog, &model.Article{ID: tid, Title: title})
+		goblog = append(goblog, &model.Article{ID: tid, Title: title, Content: content})
 	}
 	return
 }
 
 func (m *mysqlArticleRepository) Add(content string, title string) (int, error) {
-	cur := time.Now()
+
+
 	tx, err := m.db.Begin()
 	if err != nil {
 		return 0, err
 	}
 
-	result, err := tx.Exec("INSERT INTO goblog (created_at, updated_at, deleted_at, title) VALUES (?, ?, ?, ?)", cur, cur, sql.NullTime{}, title)
+	result, err := tx.Exec("INSERT INTO goblog (content, title) VALUES (?, ?)", content, title)
 	if err != nil {
 		return 0, err
 	}
@@ -96,17 +82,10 @@ func (m *mysqlArticleRepository) Del(id int) error {
 		return err
 	}
 
-	_, err = tx.Exec("DELETE FROM tour_collects WHERE tour_id = ?", id)
-	if err != nil {
-		tx.Rollback()
-		return err
-	}
-
 	_, err = tx.Exec("DELETE FROM goblog WHERE id = ?", id)
 	if err != nil {
 		tx.Rollback()
 		return err
 	}
-
 	return tx.Commit()
 }
