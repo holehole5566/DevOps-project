5a68524deec7d797eb028471ef262e04bc11dc16
holehole5566 <jd965303@gmail.com>
2021-12-27 20:48:13 +0800

    vue

--- web/src/main.js
+++ web/src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
 
-createApp(App).mount('#app')
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+Vue.use(BootstrapVue)
+
+createApp(App).use(router).mount('#app')
