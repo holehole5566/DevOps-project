a2ab403e3be8e463fff772c67226bef67c4c1998
holehole5566 <jd965303@gmail.com>
2021-12-28 21:01:30 +0800

    test

---
+++ web/src/views/Blog.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="blog">
+    <Blog msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Blog from '@/components/Blog.vue'
+
+export default {
+  name: 'Blog',
+  components: {
+    Blog
+  }
+}
+</script>
+
